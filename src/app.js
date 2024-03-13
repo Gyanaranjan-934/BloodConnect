79703c9932b9e1ff554d9e9468d0f2c167dfa6a3
Gyanaranjan Sahoo <gyanaranjansahoo509@gmail.com>
2024-03-13 12:30:43 +0530

    backend initial setup started

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express();
+
+
+// used to set the security frontend url for incoming requests
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+// set how much size of cookie can be requested to server
+app.use(express.json({ limit: "16kb" }));
+// to encode the urls and limit the query size after request
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"))
+app.use(cookieParser());
+
+// // routes import
+// import userRouter from './routes/user.routes.js'
+// import productRouter from './routes/product.routes.js'
+// import reviewRouter from './routes/rating.routes.js'
+// import chatRouter from './routes/chat.routes.js'
+
+
+// // routes declarations
+// app.use('/api/v1/users',userRouter)
+// app.use('/api/v1/products',productRouter)
+// app.use('/api/v1/reviews',reviewRouter)
+// app.use('/api/v1/chats',chatRouter)
+
+export { app }
---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+import { app } from './app.js';
+import { Server } from 'socket.io';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+    .then(() => {
+        const server = app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is listening at ${process.env.PORT}`);
+        })
+        // const io = new Server(server, {
+        //     pingTimeout: 60000,
+        //     cors: {
+        //         origin: '*', //TODO: should change the origin at deployment time
+        //         methods: ['GET', 'POST']
+        //     }
+        // });
+        // io.on('connection', (socket) => {
+        //     console.log('A user connected');
+        //     // Handle chat messages
+        //     socket.on('chat message', (message) => {
+        //         console.log("Connected to the chat");
+        //         // Broadcast the message to all connected clients
+        //         io.emit('chat message', message);
+        //     });
+        //     socket.on("typing", () => {
+        //         socket.emit("typing");
+        //     });
+        
+        //     socket.on("stop typing", (room) => {
+        //         socket.emit("stop typing");
+        //     });
+        //     // Handle disconnections
+        //     socket.on('disconnect', () => {
+        //         console.log('User disconnected');
+        //     });
+        // });
+        
+    })
+    .catch((err) => {
+        console.error(err)
+    });
